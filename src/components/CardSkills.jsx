dab7bb9d120753dacd11a3060ece913c37ab894d
DyBryck <150355330+DyBryck@users.noreply.github.com>
2024-09-20 10:04:10 +0200

    new modal + OpenGraph

--- src/components/CardSkills.jsx
+++ src/components/CardSkills.jsx
@@ -4,14 +4,22 @@ import skillsList from "../assets/data/skillsList.json";
 const CardSkills = () => {
   return (
     <div className="col-span-6 flex flex-col items-center justify-center rounded-lg bg-white/20 shadow-xl backdrop-blur-xl transition-all duration-500 hover:scale-105 hover:opacity-100 md:opacity-80 lg:col-span-3 lg:row-span-2 xl:col-span-2 xl:row-span-1">
-      <h2 className="text-3xl font-bold">Mes compétences</h2>
-      <Marquee className="h-24 cursor-pointer" pauseOnHover="true" speed={30}>
+      <h2 className="text-3xl font-bold lg:mb-4">Mes compétences</h2>
+      <Marquee
+        className="h-24 cursor-pointer lg:h-32"
+        pauseOnHover="true"
+        speed={30}
+      >
         {skillsList.map((skill) => (
           <div
             key={skill.name}
-            className="mr-12 flex h-16 w-16 items-center justify-center rounded-full bg-white/30 transition-transform duration-500 hover:scale-110"
+            className="mr-12 flex h-16 w-16 items-center justify-center rounded-full bg-white/30 transition-transform duration-500 hover:scale-110 lg:h-24 lg:w-24"
           >
-            <img src={skill.logo} alt={"Logo " + skill.name} className="h-8" />
+            <img
+              src={skill.logo}
+              alt={"Logo " + skill.name}
+              className="h-8 lg:h-16"
+            />
           </div>
         ))}
       </Marquee>
