21f3b2a18ee6b983c021ac95c679b49c42b2e8a0
DyBryck <150355330+DyBryck@users.noreply.github.com>
2024-10-01 10:53:23 +0200

    Update CardLille.jsx

--- src/components/CardLille.jsx
+++ src/components/CardLille.jsx
@@ -1,7 +1,12 @@
+import { Image } from "antd";
+
 const CardLille = () => {
   return (
     <div className="col-span-6 row-span-1 overflow-hidden rounded-lg shadow-xl backdrop-blur-xl transition-all duration-500 hover:scale-105 hover:opacity-100 md:col-span-2 md:row-span-3 md:opacity-80 lg:col-span-3 lg:row-span-2 xl:col-span-2 xl:row-span-2">
-      <img
+      <Image
+        width={"100%"}
+        height={"100%"}
+        preview={false}
         src={"./images/maps/lille.webp"}
         alt="Carte de Lille, région dans laquelle je suis prêt à me déplacer"
         className="h-full w-full object-cover transition-all duration-500 hover:scale-110"
