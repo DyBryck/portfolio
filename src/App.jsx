9013400aa802dfdb5ab5f1b0fa489480c363bc74
DyBryck <150355330+DyBryck@users.noreply.github.com>
2024-10-16 15:29:54 +0200

    update h1

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,6 @@ const App = () => {
       <ButtonToTop />
       <div className="mb-8 mt-4 flex items-center justify-center rounded-lg p-8 text-center shadow-xl backdrop-blur-xl transition-all duration-500 hover:scale-105 md:mb-16 dark:bg-black/20">
         <h1 className="rounded-lg text-2xl font-bold uppercase sm:text-3xl md:text-5xl">
-          Portfolio
-          <br />
           Dylan Bryckaert
         </h1>
       </div>
