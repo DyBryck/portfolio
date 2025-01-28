80fb2da613c1e038490b946a7d8f725c9d70ce0f
Dylan Bryckaert <dylan.bryckaert@gmail.com>
2025-01-28 13:31:48 +0100

    update dependencies

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
